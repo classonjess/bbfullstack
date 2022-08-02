7dc8f1faec8102f424cbb4d6537b8b0e385bde37
Jessica Classon <jessicaclasson4@gmail.com>
2022-08-01 22:44:14 -0600

    Update navbar.js

--- public/navbar.js
+++ public/navbar.js
@@ -1,11 +1,11 @@
-function NavBar(props){
+function NavBar(){
     return(
 <nav className="navbar navbar-expand-lg bg-dark">
     <a className="navbar-brand" href="#">BadBank</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#NavBar" aria-controls="NavBar" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse justify-content" id="NavBar">
+    <div className="collapse navbar-collapse justify-content-centre" id="NavBar">
       <ul className="navbar-nav">
         <li className="nav-item">
           <a className="nav-link" href="#/CreateAccount/">Create Account</a>
