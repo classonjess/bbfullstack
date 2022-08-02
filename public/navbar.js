602118f035a07a9078eafe96b8a82e3637e3573b
Jessica Classon <jessicaclasson4@gmail.com>
2022-08-01 23:57:34 -0600

    Update navbar.js

--- public/navbar.js
+++ public/navbar.js
@@ -1,6 +1,6 @@
 function NavBar(){
     return(
-<nav className="navbar navbar-expand-lg navbar-light bg-dark">
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <a className="navbar-brand" href="#">BadBank</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
