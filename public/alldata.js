478dbcdcf5cf8671919623c417441990e4b77688
Jessica Classon <jessicaclasson4@gmail.com>
2022-08-03 13:23:56 -0400

    Update alldata.js

--- public/alldata.js
+++ public/alldata.js
@@ -1,30 +1,19 @@
 function AllData(){
-    const [show, setShow]     = React.useState(true);
-    const [data, setData] = React.useState('');
-    const [user, setUser]     = React.useState('');
-    const [balance, setBalance] = React.useState('');
-    const [email, setEmail] = React.useState('');
-
-    React.useEffect(() => {
-        // Fetch all accounts from API
-        fetch('/account/all')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                ctx.user = data[0];
-                setBalance(data[0].balance);
-                props.setStatus('')
-                props.setShow(false)
-                setData(JSON.stringify(data));
-            });
-    }, []);
-   
-        return (
-            <div>
-                <p> {user.name}</p>
-                <p>{ctx.user.balance}</p>
-            <h5> Account members data</h5>
-            {data}
-            </div>);
-    }
+  const ctx = React.useContext(UserContext);
+      
+    return (
+      <Card
+        bgcolor="dark"
+        header="Account Information"
+        body={
+          <>
+           <p>Name: {JSON.stringify(ctx.users[0].name)}</p>
+           <p>Email: {JSON.stringify(ctx.users[0].email)}</p>
+           <p>Balance: ${JSON.stringify(ctx.users[0].balance)}</p>
+         </>
+             }
+          />
+        );
+      }
+      
     
