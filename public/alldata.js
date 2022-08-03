7301ff87ada3fee085478cbfc25e1a0f9c0a375d
Jessica Classon <jessicaclasson4@gmail.com>
2022-08-03 12:51:26 -0400

    Update alldata.js

--- public/alldata.js
+++ public/alldata.js
@@ -1,66 +1,19 @@
-function AllData(props){
-    const [show, setShow]     = React.useState(true);
-    const [status, setStatus] = React.useState('');  
-    const [user, setUser]     = React.useState('');
-    const [balance, setBalance] = React.useState('');
-  
+function AllData(){
+    const [data, setData] = React.useState('');
+
+    React.useEffect(() => {
+        // Fetch all accounts from API
+        fetch('/account/all')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setData(JSON.stringify(data));
+            });
+    }, []);
+   
     return (
-      <Card
-        bgcolor='dark'
-        header='Account information'
-        status={status}
-        body={
-          show ? (
-            <>
-              <AllDataForm
-                user={props.user}
-                setShow={setShow}
-                setStatus={setStatus}
-                setBalance={setBalance}
-              />
-            </>
-          ) : (
-            <>
-              {' '}
-              <AllDataMsg setShow={setShow} setStatus={setStatus} />
-              <p>{email}</p>
-              <p>Your current BadBank account balance is ${balance}</p>
-            </>
-          )
-        }
-      />
-    )
-  }
-  
-  function AllDataMsg(props) {
-    return (
-      <>
-  
-      </>
-    )
-  }
-  
-  function AllDataForm(props){
-    const [email, setEmail]   = React.useState('');
-    const [balance, setBalance] = React.useState(0);  
-    const ctx = React.useContext(UserContext);
-    const user = ctx.user;
-    
-      function handle(){
-      console.log(email,balance);
-      fetch(`/account/findOne/${email}`)
-      .then(response => response.text())
-      .then(text => {
-          try {
-            const data = JSON.parse(text)
-            props.setStatus('')
-            props.setShow(false)
-            props.setBalance(data.balance)
-            console.log('JSON:', data)
-          } catch(err) {
-            props.setStatus(text)
-              console.log('err:', text);
-          }
-      });
-    }
- }
+        <div>
+        <h5> Account members data</h5>
+        {data.email}
+        </div>);
+}
